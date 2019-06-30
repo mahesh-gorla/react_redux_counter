23f0418c91628ead935e47e3b64d6c2e00c5077b
Mahesh <mahesh.deploy@gmail.com>
2019-06-30 02:50:46 -0400

    'counter'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	incHandle = () => {
+		console.log('clicked');
+		this.props.dispatch({
+			type: 'INC'
+		});
+	};
+	decHandle = () => {
+		this.props.dispatch({
+			type: 'DEC'
+		});
+	};
+	render() {
+		return (
+			<div>
+				<div className='App'>
+					<header className='App-header'>
+						<span onClick={this.incHandle}>+</span>
+						{this.props.count}
+						<span onClick={this.decHandle}>-</span>
+					</header>
+				</div>
+			</div>
+		);
+	}
 }
 
-export default App;
+const mapStateToProps = state => ({
+	count: state.counter
+});
+
+export default connect(mapStateToProps)(App);
